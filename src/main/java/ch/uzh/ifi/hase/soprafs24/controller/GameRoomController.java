b352d11b7887f612e903654b1724074078161f70
Lydia Gattiker <lydiamilena.gattiker@uzh.ch>
2025-04-24 16:38:06 +0200

    only two may join

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/GameRoomController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/GameRoomController.java
@@ -15,6 +15,7 @@ import ch.uzh.ifi.hase.soprafs24.rest.dto.GameGetDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.JoinGamePostDTO;
 import ch.uzh.ifi.hase.soprafs24.service.Authenticator;
 import ch.uzh.ifi.hase.soprafs24.repository.GameRepository;
+import ch.uzh.ifi.hase.soprafs24.entity.User;
 
 @RestController
 public class GameRoomController {
@@ -71,12 +72,16 @@ public class GameRoomController {
     @PostMapping("/games/{gameId}/join")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public GameGetDTO joinGame(@PathVariable("gameId") Long id, @RequestBody JoinGamePostDTO gamePostDTO, @RequestHeader("Authorization") String authenticatorToken){
-        String token = authenticatorToken.substring(7);
+    public void joinGame(@PathVariable("gameId") Long id, @RequestBody JoinGamePostDTO gamePostDTO, @RequestHeader("Authorization") String authenticatorToken){
+        
 
+
+        String token = authenticatorToken.substring(7);
         Game game = gameService.joinGame(id, token, gamePostDTO.getPassword());
+        gameService.checkUser(token, game);
+
         GameGetDTO gameGetDTO = DTOMapper.INSTANCE.convertEntityToGameGetDTO(game);
-        return gameGetDTO;
+        return;
 
 
     }    
